e2fd4178caca5bc92eb4ecd0a96c13defd7faa40
zsyzgu <zsyzgu@163.com>
2016-03-16 00:45:45 +0800

    Net start.

---
+++ server/Assets/Scripts/client.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class client : MonoBehaviour {
+    string IP = "";
+    int port = 1234;
+    string message = "";
+
+    void OnGUI() {
+        switch (Network.peerType) {
+            case NetworkPeerType.Disconnected:
+                startConnect();
+                break;
+            case NetworkPeerType.Server:
+                break;
+            case NetworkPeerType.Client:
+                onClient();
+                break;
+            case NetworkPeerType.Connecting:
+                break;
+        }
+    }
+
+    void startConnect() {
+        IP = GUILayout.TextArea(IP);
+        
+        if (GUILayout.Button("Connect to server")) {
+            NetworkConnectionError error = Network.Connect(IP, port);
+            switch (error) {
+                case NetworkConnectionError.NoError:
+                    break;
+                default:
+                    Debug.Log("client:" + error);
+                    break;
+            }
+        }
+    }
+
+    void onClient() {
+        GUILayout.Box(message);
+        if (Input.GetButton("Fire1")) {
+            message = (float)Input.mousePosition.x / Screen.width + ", " + (float)Input.mousePosition.y / Screen.height;
+            GetComponent<NetworkView>().RPC("reciveMessage", RPCMode.All, message);
+        } else {
+            message = "-1, -1";
+            GetComponent<NetworkView>().RPC("reciveMessage", RPCMode.All, message);
+        }
+    }
+
+    [RPC]
+    void reciveMessage(string msg, NetworkMessageInfo info) {
+        message = msg;
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ server/Assets/Scripts/server.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class server : MonoBehaviour {
+    public Texture cursor;
+    int cursorSize = 40;
+    string IP = "";
+    int port = 1234;
+    string message = "-1, -1";
+
+    void OnGUI() {
+        switch (Network.peerType) {
+            case NetworkPeerType.Disconnected:
+                startServer();
+                break;
+            case NetworkPeerType.Server:
+                onServer();
+                break;
+            case NetworkPeerType.Client:
+                break;
+            case NetworkPeerType.Connecting:
+                break;
+        }
+    }
+
+    void startServer() {
+        NetworkConnectionError error = Network.InitializeServer(12, port, false);
+        switch (error) {
+            case NetworkConnectionError.NoError:
+                break;
+            default:
+                Debug.Log("Error:" + error);
+                break;
+        }
+    }
+
+    void onServer() {
+        IP = Network.player.ipAddress;
+        GUILayout.Box(IP);
+        float x = float.Parse(message.Split(',')[0]);
+        float y = float.Parse(message.Split(',')[1]);
+        if (x != -1 && y != -1) {
+            x = (1 - x) * 10 - 5;
+            y = y * 10 - 5;
+            //GUI.DrawTexture(new Rect(x, y, cursorSize, cursorSize), cursor);
+            RectTransform cursor = GameObject.Find("Canvas/cursor").GetComponent<RectTransform>();
+            cursor.position = new Vector3(x, y, 10f);
+            GameObject sphere = GameObject.Find("Sphere");
+            sphere.transform.position = new Vector3(x, y, 12f);
+        }
+    }
+
+    [RPC]
+    void reciveMessage(string msg, NetworkMessageInfo info) {
+        message = msg;
+    }
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
