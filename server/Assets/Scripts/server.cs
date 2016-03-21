1c05cd1fdfbe6c93670e444673f0e624ca705a92
zsyzgu <zsyzgu@163.com>
2016-03-21 18:40:32 +0800

    cursor test ok.

--- server/Assets/Scripts/server.cs
+++ server/Assets/Scripts/server.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using UnityEngine.VR;
 
 public class server : MonoBehaviour {
+    public GameObject canvasParent;
     public Texture cursor;
     int cursorSize = 40;
     string IP = "";
@@ -35,19 +36,20 @@ public class server : MonoBehaviour {
         }
     }
 
+    Vector3 rot = new Vector3(0.1f, 0f, 0f);
     void onServer() {
         IP = Network.player.ipAddress;
         GUILayout.Box(IP);
         
         RectTransform cursor = GameObject.Find("Canvas/cursor").GetComponent<RectTransform>();
+        canvasParent.transform.rotation = InputTracking.GetLocalRotation(VRNode.CenterEye);
         float x = float.Parse(message.Split(',')[0]);
         float y = float.Parse(message.Split(',')[1]);
         if (x != -1 && y != -1) {
             x = (1 - x) * 10 - 5;
             y = y * 10 - 5;
         }
-        cursor.localPosition = new Vector3(cursor.localPosition.x, cursor.localPosition.y, 1f);
-        cursor.localRotation = GetComponent<OVRCameraRig>().trackingSpace.localRotation;
+        cursor.localPosition = new Vector3(x, y, 1f);
     }
 
     [RPC]
